2e79ca51a08b639e5f53ddaf69f5089a096d7e1d
Tom Krenzke <krenzke@gmail.com>
2024-11-29 12:38:31 -0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <div class="container">
+    <header>
+      <h1>Simple Weather App</h1>
+      <p>It's just a weather app, keep it simple</p>
+    </header>
+
+    <main>hello</main>
+  </div>
+</template>
+
+<style scoped>
+header {
+  margin: 1rem 0;
+  & > h1 {
+    margin-bottom: 0;
+  }
+  & > p {
+    color: var(--subtext-grey);
+    font-style: italic;
+  }
+}
+</style>
